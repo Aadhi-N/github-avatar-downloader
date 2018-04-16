aec9bb20dfc3821ae0bdfaaefd1db6fb894bca68
Fathima-N <aadhilah.nizamdeen@gmail.com>
2018-04-16 19:12:06 -0400

    edits

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,28 @@
 var request = require('request');
+var request = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, callback) {
+
+	var options = {
+		url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+
+		headers: {
+			'User-Agent': 'request',
+		}
+	};
+
+	
+
+	request(options, function(err, res, body) {
+		callback(err, body);
+	});
+};
+
 
 	getRepoContributors("jquery", "jquery", function (err, result) {
 		console.log("Errors: ", err);
 
 		console.log("Results: ", result);
 	});
-}
